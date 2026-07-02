56566ae6041eb9b46790f277db962b13c70478b4
Punit Patel <pr17032006@gmail.com>
2026-07-02 16:56:20 +0530

    increase jwt token lifetime for development

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,8 +130,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
